886a8c902e28aec4ada69080dd4ad9c963d7a317
BillGatesReckoning <rusinkost@gmail.com>
2024-06-02 16:18:55 +0200

    Create function which simulates one round of game

--- script.js
+++ script.js
@@ -2,26 +2,71 @@ console.log("Hello world")
 
 let humanScore = 0
 let computerScore = 0
-
+// Rock = 1 Paper = 2 Scissors = 3
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
-    let choice = "Paper"
+    let choice = 1
     if(randomNumber === 0) {
-        choice = "Rock"
+        choice = 2
     }
     else if(randomNumber === 1) {
-        choice = "Scissors"
+        choice = 3
     }
+    console.log(choice)
     return choice
 }
 
 function getHumanChoice() {
     let choice = ""
-    while(choice != "Rock" && choice != "Paper" && choice != "Scissors") {
-        choice = prompt("Play Rock, Paper or Scissors")
-    }
+    choice = prompt("Play Rock, Paper or Scissors")
+    console.log(choice)
     return choice
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+// Rock = 1 Paper = 2 Scissors = 3
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`Tie!`)
+    }
+    else {
+        if(humanChoice == 1) {
+            switch (computerChoice) {
+                case 2:
+                    console.log(`Computer won. Paper beats rock.`)
+                    computerScore++
+                    break
+                case 3:
+                    console.log(`Human won. Rock beats scissors`)
+                    humanScore++
+                    break
+            }
+        }
+        else if (humanChoice == 2) {
+            switch (computerChoice) {
+                case 1:
+                    console.log(`Human won. Paper beats rock.`)
+                    humanScore++
+                    break
+                case 3:
+                    console.log(`Computer won. Scissors beat paper`)
+                    computerScore++
+                    break
+            }
+        }
+        else {
+            switch (computerChoice) {
+                case 1:
+                    console.log(`Computer won. Rock beats scissors.`)
+                    computerScore++
+                    break
+                case 2:
+                    console.log(`Human won. Scissors beat paper`)
+                    humanScore++
+                    break
+            }
+        }
+    }
+    return
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()))
